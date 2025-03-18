a4e5100a7af44bdbbf01c91911ac64475e74badc
sanin credot <sanin@credot.in>
2025-03-18 15:47:11 +0530

    fixed swiper build error

--- src/components/Swiper/BannerSwiper.tsx
+++ src/components/Swiper/BannerSwiper.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
-import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -11,28 +11,31 @@ type Props = {
 
 export default function BannerSwiper({ images }: Props) {
   return (
-    <>
-      <Swiper
-        pagination={{
-          dynamicBullets: true,
-        }}
-        modules={[Pagination]}
-        className="banner_image_swiper"
-      >
-        {images?.length &&
-          images?.map((image: any, index: any) => (
-            <SwiperSlide
-              key={index}
-              className="image_div"
-            >
-              <Image
-                fill
-                src={image}
-                alt={`banner ${index}`}
-              />
-            </SwiperSlide>
-          ))}
-      </Swiper>
-    </>
+    <Swiper
+      pagination={{ dynamicBullets: true }}
+      modules={[Pagination]}
+      className="banner_image_swiper"
+    >
+      {images?.map((image: string, index: number) => (
+        <SwiperSlide key={index} className="image_div">
+          <div
+            style={{
+              position: "relative",
+              width: "100%",
+              height: "400px",
+              borderRadius: "20px",
+              overflow: "hidden",
+            }}
+          >
+            <Image
+              src={image}
+              alt={`banner ${index}`}
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
